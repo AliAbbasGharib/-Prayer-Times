f9623468623b55c73d6614e4b889021773ba3a9d
Ali Gharib <125898718+AliAbbasGharib@users.noreply.github.com>
2024-10-09 14:04:48 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+
+let click = document.createElement("click");
+ function getDate()
+{
+
+    axios.get("http://api.aladhan.com/v1/timingsByCity/{date}?city=${city}&country=Lebanon&method=0")
+    .then((response) => {
+        let weekday = response.data.data.date.gregorian.weekday.en;
+        let date = response.data.data.date.gregorian.date;
+        document.getElementById("date").innerHTML = weekday + " " + date ;
+    
+    })
+}
+getDate();
+
+let cities = ["Beirut","Saida","Tyre","Marjaayoun","Baalbek","Nabatieh"];
+
+for(let city of cities)
+{
+    let content =  `
+            <select name="city" id="city">
+                    <option>${city}</option>
+                </select>
+    `
+    document.getElementById("city").innerHTML += content;
+}
+    
+
+function getTime()
+{
+    let city = document.getElementById("city").value;
+    axios.get(`http://api.aladhan.com/v1/timingsByCity/{date}?city=${city}&country=Lebanon&method=0`)
+    .then((response) => {
+       
+        let times = response.data.data.timings;
+        document.getElementById("cityName").innerHTML = city;
+        document.getElementById("fajrTime").innerHTML = times.Fajr + " AM";
+        document.getElementById("duhorTime").innerHTML = times.Dhuhr + " PM";
+        document.getElementById("aserTime").innerHTML = times.Asr + " PM";
+        document.getElementById("magribTime").innerHTML = times.Maghrib + " PM";
+        document.getElementById("ishaTime").innerHTML = times.Isha + " PM";
+    })
+    .catch((error) =>{
+        alert("There was an error fetching the timings:",error);
+    })
+}
+getTime();
